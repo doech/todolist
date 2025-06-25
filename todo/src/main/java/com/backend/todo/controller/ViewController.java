d9bd987313c94f68351b4ef26a08026ff082f1e5
María Sierra <malesierra15@gmail.com>
2025-06-25 10:22:02 -0600

    Update ViewController.java

--- todo/src/main/java/com/backend/todo/controller/ViewController.java
+++ todo/src/main/java/com/backend/todo/controller/ViewController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ViewController {
     @GetMapping("/dashboard")
     public String dashboard() {
-        return "dashboard"; 
+        return "index"; 
     }
 }
